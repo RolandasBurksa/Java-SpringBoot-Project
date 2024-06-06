436107fba071587220df6561457f8a7ec3089f24
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-06 12:47:49 +0300

    First back-end commit

---
+++ Spring project/back-end/java-springboot-project/src/main/java/lt/techin/java_springboot_project/JavaSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.java_springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ Spring project/back-end/java-springboot-project/src/test/java/lt/techin/java_springboot_project/JavaSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.java_springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
