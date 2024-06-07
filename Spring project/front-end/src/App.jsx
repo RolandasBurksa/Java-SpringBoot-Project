4a4e7b1942e4a789d776aad52ad3ee90c5dfdb02
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-07 15:39:11 +0300

    Front-end added Routing

--- Spring project/front-end/src/App.jsx
+++ Spring project/front-end/src/App.jsx
@@ -2,13 +2,21 @@ import "./App.css";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListBooksComponent from "./components/ListBooksComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListBooksComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          {/* // http://localhost:8000 */}
+          <Route path="/" element={<ListBooksComponent />}></Route>
+          {/* // http://localhost:8000/books */}
+          <Route path="/books" element={<ListBooksComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
