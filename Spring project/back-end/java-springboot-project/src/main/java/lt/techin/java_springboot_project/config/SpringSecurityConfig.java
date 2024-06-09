5cfa8798bde1dab957a1886cced14c68fc0be47b
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-09 19:34:36 +0300

    Usual commit

---
+++ Spring project/back-end/java-springboot-project/src/main/java/lt/techin/java_springboot_project/config/SpringSecurityConfig.java
@@ -0,0 +1,51 @@
+package lt.techin.java_springboot_project.config;
+
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    public UserDetailsService userDetailsService(){
+
+        UserDetails vartotojas1 = User.builder()
+                .username("vartotojas1")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode( "admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(vartotojas1, admin);
+
+    }
+}
