9b5a08d89b267f08052d12fdefa11cdd4a099253
RolandasBurksa <151562430+RolandasBurksa@users.noreply.github.com>
2024-06-12 19:45:46 +0300

    Front-end: Add Secured Routes in React App

--- Spring project/front-end/src/App.jsx
+++ Spring project/front-end/src/App.jsx
@@ -1,15 +1,27 @@
+/* eslint-disable react/prop-types */
 import "./App.css";
 import BookComponent from "./components/BookComponent";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import ListBooksComponent from "./components/ListBooksComponent";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ListCategoryComponent from "./components/ListCategoryComponent";
 import CategoryComponent from "./components/CategoryComponent";
 import RegisterComponent from "./components/RegisterComponent";
 import LoginComponent from "./components/LoginComponent";
+import { isUserLoggedIn } from "./services/AuthService";
 
 function App() {
+  function AuthenticatedRoute({ children }) {
+    const isAuth = isUserLoggedIn();
+
+    if (isAuth) {
+      return children;
+    }
+
+    return <Navigate to="/" />;
+  }
+
   return (
     <>
       <BrowserRouter>
@@ -19,19 +31,58 @@ function App() {
           {/* <Route path="/" element={<ListBooksComponent />}></Route> */}
           <Route path="/" element={<LoginComponent />}></Route>
           {/* // http://localhost:8000/books */}
-          <Route path="/books" element={<ListBooksComponent />}></Route>
+          <Route
+            path="/books"
+            element={
+              <AuthenticatedRoute>
+                <ListBooksComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
           {/* // http://localhost:8000/add-book */}
-          <Route path="/add-book" element={<BookComponent />}></Route>
+          <Route
+            path="/add-book"
+            element={
+              <AuthenticatedRoute>
+                <BookComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
           {/* // http://localhost:8000/update-book/1 */}
-          <Route path="/update-book/:id" element={<BookComponent />}></Route>
+          <Route
+            path="/update-book/:id"
+            element={
+              <AuthenticatedRoute>
+                <BookComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
           {/* // http://localhost:8000/categpries */}
-          <Route path="/categories" element={<ListCategoryComponent />}></Route>
+          <Route
+            path="/categories"
+            element={
+              <AuthenticatedRoute>
+                <ListCategoryComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
           {/* // http://localhost:8000/add-category */}
-          <Route path="/add-category" element={<CategoryComponent />}></Route>
+          <Route
+            path="/add-category"
+            element={
+              <AuthenticatedRoute>
+                <CategoryComponent />
+              </AuthenticatedRoute>
+            }
+          ></Route>
           {/* // http://localhost:8000/edit-category/1 */}
           <Route
             path="/edit-category/:id"
-            element={<CategoryComponent />}
+            element={
+              <AuthenticatedRoute>
+                <CategoryComponent />
+              </AuthenticatedRoute>
+            }
           ></Route>
           {/* // http://localhost:8000/register */}
           <Route path="/register" element={<RegisterComponent />}></Route>
